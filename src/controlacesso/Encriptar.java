79aca47f7731118c4a9b4c6a8189e568486f4989
rene <rene@rene-PC>
2017-07-17 18:42:40 -0500

    clase de incriptacion

---
+++ src/controlacesso/Encriptar.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controlacesso;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author rene
+ */
+public class Encriptar {
+
+    public static void main(String[] args) {
+        try {
+//            String encriptado = Utilidades.Encriptar("Esto es una prueba");
+//            System.out.println(encriptado);
+//            String desencriptado;
+//
+//            desencriptado = Utilidades.Desencriptar(encriptado);
+//
+//            System.out.println(desencriptado);
+        } catch (Exception ex) {
+            Logger.getLogger(Encriptar.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
+
+}
