125053791577c8eda2748cd9108a072c2998ed71
IsaMore <nyxbellafee@gmail.com>
2017-07-17 20:23:45 -0500

    Se agregó la clase gestionUsuarios para las acciones a la base de datos

--- src/gestionUsuarios.java
+++ src/gestionUsuarios.java
@@ -1,22 +1,21 @@
+
+import controlacesso.Conexion;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package controlacesso;
-import java.sql.Connection;
-import java.sql.Statement;
-import java.sql.ResultSet;
 
 /**
  *
  * @author Isabella
  */
-public class inventario {
+public class gestionUsuarios {
     private static Conexion conexion;
-    
-    public inventario (){
-    }
     
     public void consultar(){
         conexion = new Conexion();
@@ -24,46 +23,72 @@ public class inventario {
         Statement st;
         ResultSet rs;
        // System.out.println("inserte nombre");
-        String sql = "select *from inventario";
+        String sql = "select *from usuarios";
         try {
             st = con.createStatement();
             rs = st.executeQuery(sql);  
+            
+            
+            
             con.close();
             rs.close();
             st.close();
         } catch (Exception e) {
-            System.out.println("Error al consultar datos");
+            System.out.println("Error al consultar tabla de usuarios");
         }  
+        
     }
     
-    public void insertar(String nombre, int cantidad, double precio){
+    public void registar(String user, String password){
         conexion = new Conexion();
         Connection con = conexion.getConnection();
         Statement st;
-       // System.out.println("inserte nombre");
-        String sql = "insert into inventario (nombre, cantidad, precio) values ('"+nombre+"','"+cantidad+"','"+precio+"')"; 
+        String sql = "insert into users (name, password) values ('"+user+"','"+password+"')"; 
         try {
             st= con.createStatement();
             st.executeUpdate(sql);
             con.close();
             st.close();
         } catch (Exception e) {
+        }       
+    }
+    
+    public void modificar(int id, String user, String password){
+        conexion = new Conexion();
+        Connection con = conexion.getConnection();
+        Statement st;
+       // System.out.println("inserte nombre");
+        String sql = "update users set name='"+user+"','"+password+"' where iduser ="+id+""; 
+        try {
+            st = con.createStatement();
+            int confirmar = st.executeUpdate(sql);
+            if(confirmar ==1){
+                System.out.println("Los datos de usuario han sido modificados");
+            } else{
+                System.out.println("Error modificando usuario");
+            }
+            st.close();
+            con.close();
+                    
+        } catch (Exception e) {
+            System.out.println("Error "+ e);
         }
-    }   
+    
+    }
     
     public void eliminar(int id){
         conexion = new Conexion();
         Connection con = conexion.getConnection();
         Statement st;
        // System.out.println("inserte nombre");
-        String sql = "delete from inventario where idProducto="+id; 
+        String sql = "delete from users where iduser="+id; 
         try {
             st = con.createStatement();
             int confirmar = st.executeUpdate(sql);
             if(confirmar ==1){
-                System.out.println("Registro eliminado");
+                System.out.println("El usuario ha sido eliminado");
             } else{
-                System.out.println("Error al eliminar");
+                System.out.println("Error tratando de eliminar al usuario");
             }
             st.close();
             con.close();
@@ -71,31 +96,9 @@ public class inventario {
         } catch (Exception e) {
             System.out.println("Error "+ e);
         }
-   
-        
+    
     }
-    public void modificar( int id, String nombre, int cantidad, double precio){
-        conexion = new Conexion();
-        Connection con = conexion.getConnection();
-        Statement st;
-       // System.out.println("inserte nombre");
-        String sql = "update inventario set nombre='"+nombre+"','"+cantidad+"','"+precio+"' where idProducto ="+id+""; 
-        try {
-            st = con.createStatement();
-            int confirmar = st.executeUpdate(sql);
-            if(confirmar ==1){
-                System.out.println("Producto Modificado");
-            } else{
-                System.out.println("Error al modificar producto");
-            }
-            st.close();
-            con.close();
-                    
-        } catch (Exception e) {
-            System.out.println("Error "+ e);
-        }
-        
-    }
-
+    
+    
     
 }
